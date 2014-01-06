029dc4654b9d674ed0606be0e623b7330f31bd35
John-Alan Simmons <simmons.johnalan@gmail.com>
2014-01-05 19:22:17 -0500

    Finished v2, not tested. Includes cropping service

--- metavid.go
+++ metavid.go
@@ -14,41 +14,46 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
+	"strings"
+	//"time"
 )
 
 var (
 	rootDir string
 	port    int
-
-	//Arguments
-	arg0 string = "-print_format"
-	arg1 string = "json"
-	arg2 string = "-show_format"
-	arg3 string = ">"
 )
 
 type response struct {
-	Status int    `json:"status"`
-	Msg    string `json:"msg"`
+	Status int         `json:"status"`
+	Msg    interface{} `json:"msg"`
 }
 
-type MetaDataService struct{}
+type cropResponse struct {
+	Name     string `json:"new_file"`
+	Duration int    `json:"duration"`
+}
 
-func (m MetaDataService) Register() {
+type VideoService struct{}
+
+func (v VideoService) Register() {
 
 	ws := new(restful.WebService)
 
 	ws.
-		Path("/metadata").
+		Path("/").
 		Consumes(restful.MIME_JSON).
 		Produces(restful.MIME_JSON)
 
-	ws.Route(ws.GET("/{filename}").To(m.getMetaData))
+	//Metadata endoint
+	ws.Route(ws.GET("/metadata/{filename}").To(v.getMetaData))
+
+	//Video cropping
+	ws.Route(ws.POST("/crop/{filename}").To(v.cropVideo))
 
 	restful.Add(ws)
 }
 
-func (m MetaDataService) getMetaData(request *restful.Request, r *restful.Response) {
+func (v VideoService) getMetaData(request *restful.Request, r *restful.Response) {
 	filename := request.PathParameter("filename")
 	if filename == "" {
 		fmt.Println("Could not get filename")
@@ -58,25 +63,14 @@ func (m MetaDataService) getMetaData(request *restful.Request, r *restful.Respon
 
 	//create the necessary file
 	filepath := fmt.Sprintf("%v/%v", rootDir, filename)
-	//tempJSON := fmt.Sprintf("/tmp/%v.json", filename)
-	//cmdParams := fmt.Sprintf("-print_format json -show_format %v", filepath)
 
-	cmd := exec.Command("ffprobe", arg0, arg1, arg2, filepath)
-
-	// open the out file for writing
-	/*outfile, err := os.Create("./out.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer outfile.Close()*/
+	//Execute command and get the Stdout
+	cmd := exec.Command("ffprobe", "-print_format", "json", "-show_format", filepath)
 
 	stdoutPipe, err := cmd.StdoutPipe()
 	if err != nil {
 		panic(err)
 	}
-
-	//writer := bufio.NewWriter(outfile)
-	//defer writer.Flush()
 
 	err = cmd.Start()
 	if err != nil {
@@ -92,11 +86,78 @@ func (m MetaDataService) getMetaData(request *restful.Request, r *restful.Respon
 	r.Write(jsonBuffer.Bytes())
 }
 
+func (v VideoService) cropVideo(request *restful.Request, r *restful.Response) {
+	filename := request.PathParameter("filename")
+	startTime := request.QueryParameter("start")
+	duration := request.QueryParameter("end")
+
+	if filename == "" || startTime == "" || duration == "" {
+		fmt.Println("Could not get filename")
+		writeErrorResponse(r, http.StatusBadRequest, "Invalid filename")
+		return
+	}
+
+	//create the full path
+	filepath := fmt.Sprintf("%v/%v", rootDir, filename)
+	newfile := fmt.Sprintf("63726f70-%v", filename)
+	newfilepath := fmt.Sprintf("%v/%v", rootDir, newfile)
+
+	// ffmpeg -ss 00:00:30 -i Dexter.S08E01.HDTV.x264-2HD.mp4 -to 00:00:10 -c copy output-2.mp4
+	cmd := exec.Command("ffmpeg", "-ss", startTime, "-i", filepath, "-to", duration, "-c", "copy", newfilepath)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Could not crop file")
+		writeErrorResponse(r, http.StatusInternalServerError, "Could not crop file")
+		return
+	}
+
+	dur, _ := timeToSec(duration)
+
+	writeResponse(r, cropResponse{Name: newfile, Duration: dur})
+}
+
 func writeErrorResponse(r *restful.Response, status int, msg string) {
 	r.WriteHeader(status)
 	r.Header().Set("Status", strconv.Itoa(status))
 	r.WriteAsJson(response{Status: 0, Msg: msg})
 }
+
+func writeResponse(r *restful.Response, resp interface{}) {
+	r.Header().Set("Status", "200")
+	r.WriteAsJson(response{Status: 0, Msg: resp})
+}
+
+func timeToSec(t string) (int, error) {
+	tt := strings.Split(t, ":")
+	duration := 0
+	if len(tt) == 3 {
+		hh, _ := strconv.Atoi(tt[0])
+		mm, _ := strconv.Atoi(tt[1])
+		ss, _ := strconv.Atoi(tt[2])
+		duration = (60 * 60 * hh) + (60 * mm) + (ss)
+	} else {
+		return 0, errors.New("Invalid time")
+	}
+
+	return duration, nil
+}
+
+// convert string time: HH:MM:SS to time.Time
+/*func convertTime(t string) time.Time {
+	tt := strings.Split(t, ":")
+	fmt.Println(tt)
+	if len(tt) < 3 {
+		//error
+		panic("Time format not supported")
+	}
+	hh, _ := strconv.Atoi(tt[0])
+	mm, _ := strconv.Atoi(tt[1])
+	ss, _ := strconv.Atoi(tt[2])
+
+	return time.Date(1, time.January, 0, hh, mm, ss, 0, time.Local)
+}
+
+func Duration*/
 
 func init() {
 	var err error
@@ -116,8 +177,8 @@ func init() {
 }
 
 func main() {
-	metaDataService := MetaDataService{}
-	metaDataService.Register()
+	videoService := VideoService{}
+	videoService.Register()
 
 	fmt.Println("Starting server...")
 	http.ListenAndServe(":"+strconv.Itoa(port), nil)
